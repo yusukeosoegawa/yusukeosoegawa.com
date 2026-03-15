7328dedb8873a428c9cc5199dec1756fcb4741f0
Yusuke <79902735+yusukeosoegawa@users.noreply.github.com>
2026-03-16 05:47:49 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 const serif = IBM_Plex_Serif({
   subsets: ["latin"],
